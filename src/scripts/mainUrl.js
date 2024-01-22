830192eae4679f7aa510c175095e1609029836d8
imloreno <santyago.arias.sav@gmail.com>
2024-01-22 02:57:18 -0400

    Adding .env file

--- src/scripts/mainUrl.js
+++ src/scripts/mainUrl.js
@@ -1,5 +1,7 @@
-// const MAIN_URL = "http://localhost/projects/tucandera/tucandera_remake/";
-const MAIN_URL = "https://api.tucanderatours.com/";
+const MAIN_URL =
+  process.env.REACT_APP_BACKEND_URL === "prod"
+    ? "https://api.tucanderatours.com/"
+    : "http://localhost/projects/tucandera/tucandera_remake/";
 const THIS_URL = "https://tucanderatours.com/";
 
 export { THIS_URL };
